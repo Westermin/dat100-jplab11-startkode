a02720d3b91f475e5f261484834c22b85c017a95
Westermin <reaverwester@gmail.com>
2021-11-20 01:41:39 +0100

    Update SkrivBlogg.java

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,7 +1,6 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
@@ -9,7 +8,22 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		File file = new File(mappe+filnavn);
 
-		throw new UnsupportedOperationException(TODO.method());
+		String data = samling.toString();
+
+		try(FileOutputStream fos = new FileOutputStream(file);
+			BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+
+			byte[] bytes = data.getBytes();
+
+			bos.write(bytes);
+			bos.close();
+			fos.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
