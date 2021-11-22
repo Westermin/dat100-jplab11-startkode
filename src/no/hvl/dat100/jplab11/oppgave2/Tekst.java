11155a8e9a2b5bc80e63e3b18831711871c02386
Westermin <reaverwester@gmail.com>
2021-11-22 19:23:50 +0100

    Update Tekst.java

--- src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
@@ -1,54 +1,8 @@
 package no.hvl.dat100.jplab11.oppgave2;
 
-import no.hvl.dat100.jplab11.common.TODO;
+
 import no.hvl.dat100.jplab11.oppgave1.*;
-
-public class Tekst extends Innlegg {
-
-	// TODO: objektvariable
-	private String tekst;
-	protected String startTekst = "TEKST";
-	public Tekst () {
-		//this.tekst = tekst;
-	}
-	
-	public Tekst(int id, String bruker, String dato, String tekst) {
-		super(id,bruker,dato);
-		this.tekst = tekst;
-
-	}
-	
-	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		super(id,bruker,dato,likes);
-		this.tekst = tekst;
-	}
-
-
-	// gets
-	public String getTekst() {
-		return tekst;
-	}
-	// sets
-	public void setTekst(String tekst) {
-		this.tekst = tekst;
-	}
-
-	@Override
-	public String toString() {
-		return String.format(startTekst+"\n"+super.toString()+ this.tekst+"\n");
-	}
-	
-	// Metoden nedenfor er kun for valgfri oppgave 6
-	public String toHTML() {
-			
-		throw new UnsupportedOperationException(TODO.method());
-				
-	}
-}
-package no.hvl.dat100.jplab11.oppgave2;
-
 import no.hvl.dat100.jplab11.common.TODO;
-import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Tekst extends Innlegg {
 
